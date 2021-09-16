ae39bfc9b5f5696f8def4f9fb4bd9c96e02496c2
sanix-darker <s4nixd@gmail.com>
2021-09-16 10:16:36 +0200

    feat(deploy): added custom commands + zip projects as input that can differ from hostme project name

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
 # Hostme.gh-action
 
-
 `Hostme.gh-action` (https://hostme.space/) is a github action to zip and deploy your project directly on Hostme.Space
 
 
 # Requirements :
 
-
 All this gh-action need :
- - zip
  - curl
- - unzip
-
 
 # Features :
 
-
 What this ghaction do :
- - zip the project
- - post it on hostme to be deployed
- - delete it after the deployment
+ - Run your custom inputs commands
+ - Post a given zip file on a given hostme.space project
 
+# Integration example :
 
+- Example available here :
+
+- Screenshot :
+![screenshot](https://github.com/osscameroon/hostme.gh-action.example/raw/master/screenshot.png)
--- deploy.sh
+++ deploy.sh
@@ -2,41 +2,49 @@
 #
 # Hostme.gh-actions (https://hostme.space/)
 # Requirements :
-# - zip
 # - curl
-# - unzip
 #
 # Features :
-# - zip the project
-# - post it on hostme to be deployed
-# - delete it after the deployment
+# - post your zip project on hostme to be deployed
 #
 # by d4rk3r
 
 
 deploy_me(){
-    # We create our zip filename
-    zip_file=$1_hostme.zip
+    # We get the zip filename
+    zip_file=$1
 
-    echo "[-] Zipping of the project in progress..."
-    # We zip the project
-    zip -r $zip_file .
+    # We get the hostme project name 
+    # (because the github repo name can be different to hostme project name)
+    hostme_project_name=$2
+
+    # We get the api token generated on your hostme.space dashboard
+    api_token=$3
 
     echo "[-] Deployment in progress..."
     # We post/deploy the zip on hostme
-    curl -v -X POST \
+    # and save the output in response.oss file + get the response status code
+    response_status_code=$(curl -s -o response.oss \
+    -w "%{http_code}" \
+    -v -X POST \
     -H "Accept: application/json" \
-    -H "Authorization: Bearer $2" \
-    -F "file=@./$zip_file" \
-    https://hostme.space/api/websites/$1/deploy_on_push
+    -H "Authorization: Bearer $api_token" \
+    -F "file=@$zip_file" \
+    https://hostme.space/api/websites/$hostme_project_name/deploy_on_push)
 
+    # if somethingwent wrong
+    if [ $response_status_code != "200" ]; then
+        echo "[x] An error occured !"
+        cat response.txt
 
-    echo "[-] Cleaning in progress..."
-    # We delete the zip we just created
-    rm -rf $zip_file
+        exit 1
+    else
+        # if everything went fine !
+        echo "[-] Deployed successfully !"
+    fi
 }
 
-# Where $1 is the project name
-# and $2 is the API-TOKEN
-deploy_me $1 $2
-
+# Where $1 is the project zip file name
+# the $2 is the name of that project on hostme.space
+# and $3 is the API-TOKEN created on hstme.spaces
+deploy_me $1 $2 $3
