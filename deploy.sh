b88233b1b3582bb7ac55221638ad5ac372383d8e
sanix-darker <s4nixd@gmail.com>
2022-01-16 01:02:59 +0100

    feat: upgrade and simplify the hosting process

--- deploy.sh
+++ deploy.sh
@@ -11,26 +11,22 @@
 
 
 deploy_me(){
-    # We get the zip filename
-    zip_file=$1
-
-    # We get the hostme project name 
+    # We get the hostme project name
     # (because the github repo name can be different to hostme project name)
-    hostme_project_name=$2
+    project_name=$1
 
     # We get the api token generated on your hostme.space dashboard
-    api_token=$3
+    api_token=$2
 
     # We post/deploy the zip on hostme
     curl -s \
     -v -X POST \
     -H "Accept: application/json" \
     -H "Authorization: Bearer $api_token" \
-    -F "file=@$zip_file" \
-    https://hostme.space/api/websites/$hostme_project_name/deploy_on_push
+    -F "file=@$1.zip" \
+    https://hostme.space/api/websites/$project_name/deploy_on_push
 }
 
-# Where $1 is the project zip file name
-# the $2 is the name of that project on hostme.space
-# and $3 is the API-TOKEN created on hstme.spaces
-deploy_me $1 $2 $3
+# Where $1 is the project name and it will be used to zip
+# and $2 is the API-TOKEN created on hstme.spaces
+deploy_me $1 $2
